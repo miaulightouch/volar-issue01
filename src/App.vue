34a37fe2eb94b0136a65a67951c5f1963c753828
Billy Song <billy@example.com>
2021-09-24 15:19:03 +0800

    migrate: yarn v3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <span>{{ arr }}</span>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import { union } from 'lodash-es';
 
 export default defineComponent({
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  setup() {
+    return {
+      arr: union([1,1,1,1,1,1,2,2,1,1,1,3,3,3,4]),
+    }
+  },
 });
 </script>
 
